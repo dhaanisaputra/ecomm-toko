48ee758b772ace82ee46a5eb39ccd65006ad2ab9
dani saputra <mochamad.saputra@ist.id>
2024-09-25 16:52:56 +0700

    product order

--- app/Filament/Resources/OrderResource.php
+++ app/Filament/Resources/OrderResource.php
@@ -2,28 +2,135 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\OrderResource\Pages;
-use App\Filament\Resources\OrderResource\RelationManagers;
-use App\Models\Order;
 use Filament\Forms;
-use Filament\Forms\Form;
-use Filament\Resources\Resource;
 use Filament\Tables;
+use App\Models\Order;
+use App\Models\Product;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
+use Filament\Forms\Form;
 use Filament\Tables\Table;
+use Faker\Provider\ar_EG\Text;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 use Illuminate\Database\Eloquent\Builder;
+use Filament\Forms\Components\ToggleButtons;
+use App\Filament\Resources\OrderResource\Pages;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\OrderResource\RelationManagers;
+use Faker\Core\Number;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Placeholder;
+use Illuminate\Support\Number as SupportNumber;
 
 class OrderResource extends Resource
 {
     protected static ?string $model = Order::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-shopping-bag';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Group::make()->schema([
+                    Section::make('Order Information')->schema([
+                        Select::make('user_id')
+                            ->label('Customer')
+                            ->required()
+                            ->searchable()
+                            ->preload()
+                            ->relationship('user', 'name'),
+                        Select::make('payment_method')
+                            ->options(['cash' => 'Cash', 'cod' => 'Cash on Delivery'])
+                            ->required(),
+                        Select::make('payment_status')
+                            ->options(['paid' => 'Paid', 'pending' => 'Pending', 'failed' => 'Failed'])
+                            ->default('pending')
+                            ->required(),
+                        ToggleButtons::make('status')
+                            ->inline()
+                            ->options(['new' => 'New', 'processing' => 'Processing', 'shipped' => 'Shipped', 'delivered' => 'Delivered', 'canceled' => 'Canceled'])
+                            ->default('new')
+                            ->required()
+                            ->colors(['new' => 'info', 'processing' => 'warning', 'shipped' => 'success', 'delivered' => 'success', 'canceled' => 'danger'])
+                            ->icons([
+                                'new' => 'heroicon-m-sparkles',
+                                'processing' => 'heroicon-m-arrow-path',
+                                'shipped' => 'heroicon-m-truck',
+                                'delivered' => 'heroicon-m-check-badge',
+                                'canceled' => 'heroicon-m-x-circle',
+                            ]),
+                        Select::make('currency')
+                            ->options(['idr' => 'IDR', 'usd' => 'USD'])
+                            ->default('idr')
+                            ->required(),
+                        Select::make('shipping_method')
+                            ->options(['jne' => 'JNE', 'jnt' => 'JnT', 'pos' => 'POS']),
+                        Textarea::make('note')
+                            ->columnSpanFull(),
+                    ])->columns(2),
+
+                    Section::make('Order Items')->schema([
+                        Repeater::make('items')
+                            ->relationship()
+                            ->schema([
+                                Select::make('product_id')
+                                    ->label('Product')
+                                    ->required()
+                                    ->searchable()
+                                    ->preload()
+                                    ->relationship('product', 'name')
+                                    ->distinct()
+                                    ->disableOptionsWhenSelectedInSiblingRepeaterItems()
+                                    ->columnSpan(4)
+                                    ->reactive()
+                                    ->afterStateUpdated(fn($state, Set $set) => $set('unit_amount', Product::find($state)?->price ?? 0))
+                                    ->afterStateUpdated(fn($state, Set $set) => $set('total_amount', Product::find($state)?->price ?? 0)),
+                                TextInput::make('quantity')
+                                    ->numeric()
+                                    ->required()
+                                    ->default(1)
+                                    ->minValue(1)
+                                    ->columnSpan(2)
+                                    ->reactive()
+                                    ->afterStateUpdated(fn($state, Set $set, Get $get) => $set('total_amount', $state * $get('unit_amount'))),
+                                TextInput::make('unit_amount')
+                                    ->numeric()
+                                    ->required()
+                                    ->disabled()
+                                    ->dehydrated()
+                                    ->columnSpan(3),
+                                TextInput::make('total_amount')
+                                    ->numeric()
+                                    ->required()
+                                    ->dehydrated()
+                                    ->columnSpan(3),
+                            ])->columns(12),
+
+                        Placeholder::make('grand_total_placeholder')
+                            ->label('Grand Total')
+                            ->content(function (Get $get, Set $set) {
+                                $total = 0;
+                                if (!$repeaters = $get('items')) {
+                                    return $total;
+                                }
+                                foreach ($repeaters as $key => $repeater) {
+                                    $total += $get("items.{$key}.total_amount");
+                                }
+                                $set('grand_total', $total);
+                                return SupportNumber::currency($total, 'IDR');
+                            }),
+
+                        Hidden::make('grand_total')
+                            ->default(0)
+                    ])
+                ])->columnSpanFull(),
             ]);
     }
 
