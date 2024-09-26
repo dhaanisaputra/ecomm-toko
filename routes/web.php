43e3807bc5782f18cc7a8c1aac4d63c3d15b1112
dani saputra <mochamad.saputra@ist.id>
2024-09-26 15:53:18 +0700

    add tailwind and livewire

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
 
+use App\Livewire\HomePage;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', HomePage::class);
 
 // crendential login
 // user: admintoko@gmail.com
