3a0f7ff21877e392664a80f08d759b7e88c7850d
dani saputra <mochamad.saputra@ist.id>
2024-09-27 16:36:43 +0700

    cart pages

--- app/Livewire/CartPages.php
+++ app/Livewire/CartPages.php
@@ -3,9 +3,43 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\helper\CartManagement;
+use Livewire\Attributes\Title;
+use App\Livewire\Partials\Navbar;
 
+#[Title('Cart Pages - Ecomm Toko')]
 class CartPages extends Component
 {
+    public $cart_items = [];
+    public $grand_total;
+
+    public function mount()
+    {
+        $this->cart_items = CartManagement::getCartItemsFromCookie();
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+    }
+
+    public function removeCartItem($product_id)
+    {
+        $this->cart_items = CartManagement::removeCartItem($product_id);
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+        $this->dispatch('update-cart-count', total_count: count($this->cart_items))->to(Navbar::class);
+    }
+
+    public function increaseQty($product_id)
+    {
+        $this->cart_items = CartManagement::incrementQuantityToCartItem($product_id);
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+        // $this->dispatch('update-cart-count', total_count: count($this->cart_items))->to(Navbar::class);
+    }
+
+    public function decreaseQty($product_id)
+    {
+        $this->cart_items = CartManagement::decrementQuantityToCartItem($product_id);
+        $this->grand_total = CartManagement::calculateGrandTotal($this->cart_items);
+        // $this->dispatch('update-cart-count', total_count: count($this->cart_items))->to(Navbar::class);
+    }
+
     public function render()
     {
         return view('livewire.cart-pages');
