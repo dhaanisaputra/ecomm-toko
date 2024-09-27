64470c75c57b12bf23732ebc14db4bc8433ed6e3
dani saputra <mochamad.saputra@ist.id>
2024-09-27 10:54:02 +0700

    dynamic home page

--- app/Livewire/HomePage.php
+++ app/Livewire/HomePage.php
@@ -2,12 +2,22 @@
 
 namespace App\Livewire;
 
+use App\Models\Brand;
+use App\Models\Category;
 use Livewire\Component;
+use Livewire\Attributes\Title;
 
+
+#[Title('HomePage - Ecomm Toko')]
 class HomePage extends Component
 {
     public function render()
     {
-        return view('livewire.home-page');
+        $brand = Brand::where('is_active', 1)->get();
+        $categories = Category::where('is_active', 1)->get();
+        return view('livewire.home-page', [
+            'brand' => $brand,
+            'categories' => $categories,
+        ]);
     }
 }
