6ea94bea84195cfc58ae834eb584f45bbb16e21c
dani saputra <mochamad.saputra@ist.id>
2024-09-30 14:31:57 +0700

    place order page with validation

--- app/Livewire/CheckoutPages.php
+++ app/Livewire/CheckoutPages.php
@@ -3,11 +3,57 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\helper\CartManagement;
+use Livewire\Attributes\Title;
 
+#[Title('Checkout')]
 class CheckoutPages extends Component
 {
+
+    public $first_name;
+    public $last_name;
+    public $phone;
+    public $street_address;
+    public $city;
+    public $state;
+    public $zip_code;
+    public $payment_method;
+
+    public function placeOrder()
+    {
+        $this->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'phone' => 'required',
+            'street_address' => 'required',
+            'city' => 'required',
+            'state' => 'required',
+            'zip_code' => 'required',
+            'payment_method' => 'required',
+        ]);
+        $cart_items = CartManagement::getCartItemsFromCookie();
+        $grand_total = CartManagement::calculateGrandTotal($cart_items);
+        $this->emit('placeOrder', [
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'phone' => $this->phone,
+            'street_address' => $this->street_address,
+            'city' => $this->city,
+            'state' => $this->state,
+            'zip_code' => $this->zip_code,
+            'payment_method' => $this->payment_method,
+            'grand_total' => $grand_total,
+            'cart_items' => $cart_items,
+        ]);
+    }
+
     public function render()
     {
-        return view('livewire.checkout-pages');
+        $cart_items = CartManagement::getCartItemsFromCookie();
+        $grand_total = CartManagement::calculateGrandTotal($cart_items);
+        return view('livewire.checkout-pages', [
+            'cart_items' => $cart_items,
+            'grand_total' => $grand_total,
+        ]);
     }
 }
